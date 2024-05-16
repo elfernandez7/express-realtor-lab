dd10124c31f60e7df61bbf3d63bd512b21cf9bd9
elfernandez7 <efernandez.contact@gmail.com>
2024-05-16 15:48:46 -0400

    added fetch req/front end function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,71 @@
-console.log('js is linked');
+//State
+let apartments;
+let houses;
+
+//Cached Elements
+const listingsEl = document.getElementById('listings');
+
+//Event Listeners
+document.getElementById('aptsBtn').addEventListener('click', function(evt){
+    evt.preventDefault();
+    console.log('Show Apartments');
+    fetchApartments();
+});
+
+document.getElementById('houseBtn').addEventListener('click', function(evt){
+    evt.preventDefault();
+    console.log('Show Houses');
+    fetchHouses();
+});
+
+//Functions
+
+async function fetchApartments() {
+    const response = await fetch('http://localhost:3000/apartments');
+    const apartments = await response.json();
+    console.log(apartments);
+    render();
+
+
+function render() {
+    listingsEl.innerHTML = ''
+    apartments.forEach((apartment) => {
+        const cardEl = document.createElement('div');
+        cardEl.innerHTML = `<h3>${apartment.address}</h3>`;
+        const listEl = document.createElement('ul');
+        listEl.innerHTML = `
+        <li>Apartment for Rent</li>
+        <li>${apartment.beds} bed</li>
+        <li>${apartment.baths} bath</li>
+        <li>${apartment.sqft} SQFT</li>
+        <li>$ ${apartment.price}</li>
+        `;
+        cardEl.append(listEl);
+        listingsEl.append(cardEl);
+    });
+}}   
+
+async function fetchHouses() {
+    const response = await fetch('http://localhost:3000/houses');
+    const houses = await response.json();
+    console.log(houses);
+    render();
+
+
+function render() {
+    listingsEl.innerHTML = ''
+    houses.forEach((house) => {
+        const cardEl = document.createElement('div');
+        cardEl.innerHTML = `<h3>${house.address}</h3>`;
+        const listEl = document.createElement('ul');
+        listEl.innerHTML = `
+        <li>House for Sale</li>
+        <li>${house.beds} bed</li>
+        <li>${house.baths} bath</li>
+        <li>${house.sqft} SQFT</li>
+        <li>$ ${house.price}</li>
+        `;
+        cardEl.append(listEl);
+        listingsEl.append(cardEl);
+    });
+}}   
